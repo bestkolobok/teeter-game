a041fc8e0de4ec174c1eb5050eaf30a1b45dc2c7
Alex Guzey <alex.guzey@gmail.com>
2020-12-28 02:57:44 +0200

    update nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,8 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
   router: {
-    base: process.env.NODE_ENV  === 'production' ? '/edicasoft-test/' : '/'
+    base: '/'
+    // base: process.env.NODE_ENV  === 'production' ? '/test/' : '/'
   },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
