c0be5656480a9c2b843a1c9d635920ab8aa464ff
Alex Guzey <alex.guzey@gmail.com>
2020-12-28 03:08:47 +0200

    add gitignore

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -16,7 +16,7 @@ Vue.use(Router)
 
 export const routerOptions = {
   mode: 'history',
-  base: decodeURI('/'),
+  base: decodeURI('/teeter-test/'),
   linkActiveClass: 'nuxt-link-active',
   linkExactActiveClass: 'nuxt-link-exact-active',
   scrollBehavior,
